2d7df12ae90e4113622ba84d6759767055179471
0N1X9 <0n1x9@tuta.io>
2026-08-08 00:38:17 +0100

    feat: automatically create free subscriptions

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,12 +3,17 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from core.models import BusinessProfile
+from payments.models import Subscription
 
 
 @receiver(post_save, sender=User)
-def create_business_profile(sender, instance, created, **kwargs):
+def create_user_related_objects(sender, instance, created, **kwargs):
     if created:
         BusinessProfile.objects.create(
             user=instance,
             business_name=f"{instance.username}'s Business",
+        )
+
+        Subscription.objects.create(
+            user=instance,
         )
